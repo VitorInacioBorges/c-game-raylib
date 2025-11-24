0aa8e22c2eb720fdcb060889512836d5734ba816
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-11-24 16:33:30 -0300

    feat: added animations and game over screen

--- src/core/game.c
+++ src/core/game.c
@@ -2,10 +2,28 @@
 #include "../config/config.h"
 #include "game.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 
+// criando a struct da animação aqui por que o game.h nao tem include de raylib
+
+typedef struct {
+    Texture2D texture[10];
+    int frame_count;
+    int current_frame;
+    int frame_width;
+    int frame_height;
+    int active;
+    float frame_time;
+    float timer;
+    float start_delay;
+    float start_timer;
+} Animation;
+
+Animation animation[3];
 SlotState slot_state;
+GameState game_state;
 
 int money;
 int reels[3];
@@ -30,6 +48,7 @@ void InitGame() {
 
     money = INITIAL_MONEY;
     slot_state = SLOT_STATIC;
+    game_state = GAMEPLAY;
 
     reels[0] = rand() % FRUITS;
     reels[1] = rand() % FRUITS;
@@ -37,15 +56,62 @@ void InitGame() {
 
     spin_timer = 0;
 
-     /* Assets are located at ../assets relative to the build/ executable directory. 
-         Use one level up ("..") because the binary is placed in the project's build/ folder. */
-     slot_machine = LoadTexture("../assets/sprites/slotMachine.png");
-     fruit1       = LoadTexture("../assets/sprites/crucifix.png");
-     fruit2       = LoadTexture("../assets/sprites/seven.png");
-     fruit3       = LoadTexture("../assets/sprites/skull.png");
+    slot_machine = LoadTexture("../assets/sprites/slotMachine.png");
+    fruit1       = LoadTexture("../assets/sprites/crucifix.png");
+    fruit2       = LoadTexture("../assets/sprites/seven.png");
+    fruit3       = LoadTexture("../assets/sprites/skull.png");
 
-     roulette_sound  = LoadSound("../assets/audio/roleta.mp3");
-     game_over_sound = LoadSound("../assets/audio/gameover.mp3");
+    roulette_sound  = LoadSound("../assets/audio/roleta.mp3");
+    game_over_sound = LoadSound("../assets/audio/gameover.mp3");
+
+    InitAnimation(0, "../assets/sprites/animation/mistery", 10, 0.10f, 0.0f);
+    InitAnimation(1, "../assets/sprites/animation/mistery", 10, 0.10f, 0.3f);
+    InitAnimation(2, "../assets/sprites/animation/mistery", 10, 0.10f, 0.6f);
+}
+
+void InitAnimation(int i, const char *path, int frame_count, float frame_time, float delay){
+    animation[i].frame_count = frame_count;
+    animation[i].current_frame = 0;
+    animation[i].frame_time = frame_time;
+    animation[i].timer = 0;
+    animation[i].active = 1;
+    animation[i].start_delay = delay;
+    animation[i].start_timer = 0;
+
+    for (int f = 1; f <= frame_count; f++) {
+        char file[256];
+        snprintf(file, sizeof(file), "%s%d.png", path, f);
+        animation[i].texture[f-1] = LoadTexture(file);
+    }
+}
+
+void UpdateAnimation(){
+    for(int i = 0; i < 3; i++){
+        if(!animation[i].active) continue;
+
+        if (animation[i].start_timer < animation[i].start_delay) {
+            animation[i].start_timer += GetFrameTime();
+            continue;
+        }
+
+        animation[i].timer += GetFrameTime();
+
+        if(animation[i].timer >= animation[i].frame_time){
+
+            animation[i].timer = 0;
+            animation[i].current_frame++;
+
+            if(animation[i].current_frame >= animation[i].frame_count) {
+                animation[i].current_frame = 0;
+            }
+        }
+    }
+}
+
+void DrawAnimation(int i,int x, int y) {
+    if (!animation[i].active) return;
+
+    DrawTexture(animation[i].texture[ animation[i].current_frame ], x, y, WHITE);
 }
 
 void DrawFruit(int fruit, int x, int y, int offsetX, int offsetY) {
@@ -65,8 +131,41 @@ void DrawFruit(int fruit, int x, int y, int offsetX, int offsetY) {
     }
 }
 
+void DrawGameOver() {
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    DrawText("VOCÊ EMPOBRECEU!!", 70, 150, 25, RED);
+
+    Rectangle btn = { 120, 200, 160, 50 };
+
+    Color color = WHITE;
+
+    if (CheckCollisionPointRec(GetMousePosition(), btn)) color = GRAY;
+
+    DrawRectangleRec(btn, color);
+    DrawText("TRY AGAIN", btn.x + 10, btn.y + 18, 25, BLACK);
+
+    EndDrawing();
+
+    // clique no botão
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+        CheckCollisionPointRec(GetMousePosition(), btn)) {
+
+        game_over = 0;
+        slot_state = SLOT_STATIC;
+        money = INITIAL_MONEY;
+        game_state = GAMEPLAY;  // reinicia tudo
+    }
+}
+
 void DrawGame() {
 
+    if (game_state == GAME_OVER) {
+        DrawGameOver();
+        return;
+    }
     BeginDrawing();
 
     ClearBackground(BLACK);
@@ -84,28 +183,33 @@ void DrawGame() {
 
     DrawTexture(slot_machine, mx + shakeX, my + shakeY, WHITE);
 
-    int slotX = mx + 125;
-    int slotY = my + 118;
+    int slotX = mx + 130;
+    int slotY = my + 123;
     int spacing = 55;
-
-    for (int i = 0; i < 3; i++) {
-        DrawFruit(reels[i], slotX + i * spacing, slotY, shakeX, shakeY);
+    
+    if (slot_state != SLOT_SPINNING) {
+        for (int i = 0; i < 3; i++) {
+            DrawFruit(reels[i], slotX + i * spacing, slotY, shakeX, shakeY);
+        }
     }
 
-    DrawText(TextFormat("Dinheiro: %d", money), 20, 20, 25, WHITE);
+    if (slot_state == SLOT_SPINNING) {
+        for (int i = 0; i < 3; i++) {
+            DrawAnimation(i, slotX + i * spacing, slotY);
+        }
+    }
+
+    DrawText(TextFormat("Dinheiro: %d", money), 20, 360, 25, WHITE);
 
     if (lose_message_timer > 0){
         DrawText("PERDEU!!", WINDOW_WIDTH/2 - 40, WINDOW_HEIGHT/2 + 120, 20, WHITE);
-    }
-    if (game_over){
-        DrawText("GAME OVER", WINDOW_WIDTH/2 - 80, WINDOW_HEIGHT/2 - 160, 30, WHITE);
     }
 
     EndDrawing();
 }
 
 void WinGame() {
-    money += 4000;
+    money += GAIN_MONEY;
 }
 
 void LoseGame() {
@@ -116,14 +220,26 @@ void LoseGame() {
 void StartSpin() {
     if (money <= 0) return;
 
-    money -= 500;
+    money -= WITHDRAW_MONEY;
     slot_state = SLOT_SPINNING;
     spin_timer = 11.0f;
+    
+    for (int i = 0; i < 3; i++) {
+        animation[i].active = 1;
+        animation[i].current_frame = 0;
+        animation[i].timer = 0;
+        animation[i].start_timer = 0;
+    }
 
     PlaySound(roulette_sound);
 }
 
 void UpdateGame() {
+
+    if (game_state == GAME_OVER) return;
+
+    UpdateAnimation();
+
     if (lose_message_timer > 0){
         lose_message_timer -= GetFrameTime();
     }
@@ -132,6 +248,7 @@ void UpdateGame() {
     }
 
     switch(slot_state){
+
         case SLOT_STATIC:
             if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
                 StartSpin();
@@ -147,6 +264,8 @@ void UpdateGame() {
             if(spin_timer <= 0) {
                 slot_state = SLOT_RESULT;
 
+                for (int i = 0; i < 3; i++) animation[i].active = 0;
+
                 if (reels[0] == reels[1] && reels[1] == reels[2]){
                     WinGame();
                 } else {
@@ -156,8 +275,15 @@ void UpdateGame() {
 
         case SLOT_RESULT:
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !game_over) {
+
+                if (money <= 0) {
+                    game_over = 1;
+                    game_state = GAME_OVER;
+                    PlaySound(game_over_sound);
+                    break;
+                }
+
                 slot_state = SLOT_STATIC;
-            }
-            break;
+            } break;
     }
 }
--- src/core/game.h
+++ src/core/game.h
@@ -6,13 +6,22 @@
 typedef enum {
     SLOT_STATIC,     
     SLOT_SPINNING,  
-    SLOT_RESULT     
+    SLOT_RESULT,  
 } SlotState;
+
+typedef enum {
+    GAMEPLAY,
+    GAME_OVER
+} GameState;
 
 void InitGame();
 void UpdateGame();
 void DrawFruit(int fruit, int x, int y, int offsetX, int offsetY);
+void DrawGameOver();
 void DrawGame();
+void InitAnimation(int i, const char *path, int frameCount, float frameTime, float delay);
+void UpdateAnimation();
+void DrawAnimation(int i, int x, int y);
 void StartSpin();
 void WinGame();
 void LoseGame();
